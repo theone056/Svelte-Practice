223a4963999aaa9e37704c446bbcb04ed63c13b3
theone056 <32253665+theone056@users.noreply.github.com>
2022-02-09 21:15:15 +0800

    add new design to feedbackform

--- SveltePractice/src/components/Button.svelte
+++ SveltePractice/src/components/Button.svelte
@@ -16,9 +16,25 @@
         width: 100px;
         height: 40px;
         cursor: pointer;
+        position: absolute;
+        margin-top: 4px;
+        right: 125px;
     }
 
     .primary{
         background-color: #202142;
     }
+
+    button:hover{
+        transform: scale(0.98);
+        opacity: 0.9;
+    }
+
+
+    button:disabled{
+        background-color: #CCCCCC;
+        color:#333;
+        cursor: auto;
+    }
+
 </style>
--- SveltePractice/src/components/FeedbackForm.svelte
+++ SveltePractice/src/components/FeedbackForm.svelte
@@ -1,19 +1,59 @@
 <script>
     import Button from "./Button.svelte";
     import Card from "./Card.svelte";
+
+    let text = '';
+    let disabledButton = true;
+    let min = 10;
+    let message = '';
+    const InputHandler = () =>{
+        if(text.trim().length <=min)
+        {
+            message = `Text must be at least ${min} characters`
+            disabledButton = true;
+        }
+        else{
+            message = null;
+            disabledButton = false;
+        }
+    }
+
 </script>
 
 <Card>
     <header>
-        <h3>How would you rate our service ?</h3>
+        <h3>How would you rate your service ?</h3>
     </header>
     <form action="">
         <div class="input-group">
-            <input type="text" placeholder="Tell us something that keeps you coming back">
-            <Button>Send</Button>
+            <input type="text" on:input={InputHandler} bind:value={text} placeholder="Tell us something that keeps you coming back">
+            <Button disabled={disabledButton} type='submit'>Send</Button>
         </div>
+        {#if message}
+            <div class="class">
+                {message}
+            </div>
+        {/if}
     </form>
 </Card>
 
 <style>
+    header{
+        text-align: center;
+    }
+    form{
+        text-align: center;
+    }
+    .input-group{
+        text-align: center;
+        margin: 0 70px;
+    }
+
+    input{
+        width: 100%;
+        max-width: 600px;
+        border: 1px solid #C3C3E4;
+        border-radius: 8px;
+        padding: 15px;
+    }
 </style>
